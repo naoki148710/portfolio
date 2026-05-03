b3e0de82f3bf6b11f275970fcde14b8539d83586
naoki148710 <naoki2906@outlook.jp>
2026-05-03 07:41:19 +0000

    Set up routes and views]

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,5 +28,10 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/', [PostController::class, 'index'])->name('index');
+
+// PostControllerのindexメソッドを呼び出すルートを定義。URLは/posts、HTTPメソッドはGET、ルート名はposts.index。
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 
 require __DIR__ . '/auth.php';
