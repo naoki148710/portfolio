4100238098f76a44318040c010f80a89ffa60e61
naoki148710 <naoki2906@outlook.jp>
2026-06-07 05:56:24 +0000

    Remove API questions from report index

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,30 +15,9 @@ class PostController extends Controller
         // with(['user', 'student'])は、Postモデルと関連するUserモデルとStudentモデルを同時取得。N+1問題を回避。
         $posts = Post::with(['user', 'student'])->get();
 
-        // クライアントインスタンス生成
-        $client = new \GuzzleHttp\Client(
-            ['verify' => config('app.env') !== 'local'],
-        );
-
-        // GET通信するURL
-        $url = 'https://teratail.com/api/v1/questions';
-
-        // リクエスト送信と返却データの取得
-        // Bearerトークンにアクセストークンを指定して認証を行う
-        $response = $client->request(
-            'GET',
-            $url,
-            ['Bearer' => config('services.teratail.token')]
-        );
-
-        // API通信で取得したデータはjson形式なので
-        // PHPファイルに対応した連想配列にデコードする
-        $questions = json_decode($response->getBody(), true);
-
         // index bladeに取得したデータを渡す
         return view('posts.index')->with([
             'posts' => $post->getPaginateByLimit(),
-            'questions' => $questions['questions'],
         ]);
     }
 
